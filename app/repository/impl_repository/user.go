444caf659cccd207962f278b19e40bf07a792d27
kyosu-1 <sho013039@gmail.com>
2022-09-25 07:24:34 +0900

    impl user repository

---
+++ app/repository/impl_repository/user.go
@@ -0,0 +1,40 @@
+package impl_repository
+
+import (
+	"context"
+
+	"github.com/hackathon-22-digicon-team4/comiQ-server/app/dao"
+	"github.com/hackathon-22-digicon-team4/comiQ-server/app/dao/parser"
+	"github.com/hackathon-22-digicon-team4/comiQ-server/app/model"
+	"github.com/hackathon-22-digicon-team4/comiQ-server/app/repository"
+)
+
+func (r *Repository) Create(ctx context.Context, user model.User) error {
+	txn, err := r.db.BeginRWTx(ctx)
+	if err != nil {
+		return err
+	}
+	defer txn.Rollback()
+	// ユーザーの存在チェック
+	_, err = dao.SelectOneUserByID(ctx, txn, user.ID)
+	if err == nil {
+		return repository.ErrAlreadyExists
+	}
+	if err := dao.InsertUser(ctx, txn, parser.UserModel{User: user}.ToDao()); err != nil {
+		return err
+	}
+	return txn.Commit()
+}
+
+func (r *Repository) FindByID(ctx context.Context, id string) (model.User, error) {
+	txn, err := r.db.BeginROTx(ctx)
+	if err != nil {
+		return model.User{}, err
+	}
+	defer txn.Rollback()
+	user, err := dao.SelectOneUserByID(ctx, txn, id)
+	if err != nil {
+		return model.User{}, err
+	}
+	return parser.User(&user), nil
+}

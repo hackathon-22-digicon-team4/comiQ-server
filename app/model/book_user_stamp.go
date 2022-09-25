07b13e3865b5463f77aa84e35785492a94b354df
kyosu-1 <sho013039@gmail.com>
2022-09-25 15:23:39 +0900

    add NewBookUserStamp

--- app/model/book_user_stamp.go
+++ app/model/book_user_stamp.go
@@ -2,19 +2,34 @@ package model
 
 import (
 	"fmt"
+
+	"github.com/google/uuid"
 )
 
 type BookUserStamp struct {
-	ID      string
-	BookID  string
+	ID           string
+	BookID       string
 	BookSeriesID string
-	PageNum int
-	X       int
-	Y       int
-	UserID  string
-	StampID string
+	PageNum      int
+	X            int
+	Y            int
+	UserID       string
+	StampID      string
 }
 
 func (b BookUserStamp) BookPageImageURL(assetHost string) string {
 	return fmt.Sprintf("https://%s/books/%s/%d.jpg", assetHost, b.BookID, b.PageNum)
+}
+
+func NewBookUserStamp(bookID, bookSeriesID string, pageNum, x, y int, userID, stampID string) BookUserStamp {
+	return BookUserStamp{
+		ID:           uuid.New().String(),
+		BookID:       bookID,
+		BookSeriesID: bookSeriesID,
+		PageNum:      pageNum,
+		X:            x,
+		Y:            y,
+		UserID:       userID,
+		StampID:      stampID,
+	}
 }

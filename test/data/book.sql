5d4f8fb58d3033e34b4c550eb7f2c85958b44781
kyosu-1 <sho013039@gmail.com>
2022-09-25 11:00:06 +0900

    add seed book data

---
+++ test/data/book.sql
@@ -0,0 +1,15 @@
+INSERT
+    IGNORE INTO `books` (id, title, book_series_id, total_pages)
+VALUES
+    (
+        '0a7259c4-977b-4cf5-a230-42662cb20d16',
+        'gehogeho',
+        '0f013f30-e6b6-4f32-a2b7-7ef7f6508fcf',
+        100
+    ),
+    (
+        'c6ee816d-8c19-457c-9855-ffe53df488ea',
+        'hogehoge',
+        '5fbf13cf-d4ec-45b6-91ae-41ef9fc0faf6',
+        150
+    );

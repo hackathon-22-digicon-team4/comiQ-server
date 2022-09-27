7e851e35a760970f6d0f950a437a93cf146fe7d1
kyosu-1 <sho013039@gmail.com>
2022-09-28 07:20:04 +0900

    delete

--- entrypont.sh
+++
@@ -1,8 +0,0 @@
-# Secret Managerから文字列で渡ってくる環境変数jsonをパースして、環境変数にいれる
-if [ -n "$JSON_FROM_SECRET_MANAGER_STR" ]
-then
-    echo ${JSON_FROM_SECRET_MANAGER_STR} | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' > /tmp/secrets.env
-    eval $(cat /tmp/secrets.env | sed 's/^/export /')
-    rm -f /tmp/secrets.env
-fi
-

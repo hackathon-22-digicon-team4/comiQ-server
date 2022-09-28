e89d3af98ecd1b66707f3f6dfb36259ea020687e
kyosu-1 <sho013039@gmail.com>
2022-09-28 13:15:12 +0900

    add SelectStampsByIDs dao

--- app/dao/stamp.go
+++ app/dao/stamp.go
@@ -27,3 +27,26 @@ func SelectAllStamps(ctx context.Context, tx *sql.Tx) ([]*daocore.Stamp, error) 
 	}
 	return res, nil
 }
+
+func SelectStampsByIDs(ctx context.Context, tx *sql.Tx, IDs []string) ([]*daocore.Stamp, error) {
+	query := squirrel.Select(daocore.StampAllColumns...).
+		From(daocore.StampTableName).
+		Where(squirrel.Eq{"id": IDs})
+	sql, params, err := query.ToSql()
+	if err != nil {
+		return nil, err
+	}
+	rows, err := tx.QueryContext(ctx, sql, params...)
+	if err != nil {
+		return nil, err
+	}
+	res := make([]*daocore.Stamp, 0)
+	for rows.Next() {
+		r := new(daocore.Stamp)
+		if err := rows.Scan(r.Ptrs()...); err != nil {
+			return nil, err
+		}
+		res = append(res, r)
+	}
+	return res, nil
+}

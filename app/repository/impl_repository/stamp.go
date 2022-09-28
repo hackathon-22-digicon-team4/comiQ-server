dd18c23b2f22dafa93faa853a88a623fe50addde
kyosu-1 <sho013039@gmail.com>
2022-09-28 13:15:37 +0900

    FindStampsByIDs Repository

--- app/repository/impl_repository/stamp.go
+++ app/repository/impl_repository/stamp.go
@@ -24,3 +24,20 @@ func (r *Repository) FindAllStamps(ctx context.Context) ([]model.Stamp, error) {
 	}
 	return res, nil
 }
+
+func (r *Repository) FindStampsByIDs(ctx context.Context, IDs []string) ([]model.Stamp, error) {
+	txn, err := r.db.BeginROTx(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer txn.Rollback()
+	stamps, err := dao.SelectStampsByIDs(ctx, txn, IDs)
+	if err != nil {
+		return nil, err
+	}
+	res := make([]model.Stamp, 0)
+	for _, s := range stamps {
+		res = append(res, parser.Stamp(s))
+	}
+	return res, nil
+}
